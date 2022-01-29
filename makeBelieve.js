7437c7cc7272ad270ce176c2db521d5eae049f4c
palli <palli720@gmail.com>
2022-01-29 22:44:58 +0000

    skeleton

---
+++ makeBelieve.js
@@ -0,0 +1,69 @@
+class __ {
+    constructor(query) {
+        this.query = query;
+
+    }
+
+
+    //we need to return 'this' in every method in order to make them chainable, 
+    //in some methods we probably want to return a list of instance references instead
+    parent() {
+        //return this;
+    }
+
+    grandParent() {
+        //return this;
+    }
+
+    ancestor() {
+        //return this;
+    }
+
+    onClick() {
+        //return this;
+    }
+
+    insertText() {
+        //return this;
+    }
+
+    append() {
+        //return this;
+    }
+
+    prepend() {
+        //return this;
+    }
+
+    delete() {
+        //return this;
+    }
+
+    ajax() {
+        //return this;
+    }
+
+    css() {
+        //return this;
+    }
+
+    toggleClass() {
+        //return this;
+    }
+
+    onSubmit() {
+        //return this;
+    }
+
+    onInput() {
+        //return this;
+    }
+
+
+}
+
+
+
+var ut = new __('t');
+
+ut.test().test2();
